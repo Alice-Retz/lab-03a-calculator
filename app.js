65dd9b16f5551277823164f0e628aeb61dabe3ba
Alice-Retz <alicegretz@gmail.com>
2021-06-24 15:05:30 -0500

    Addition function added.

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 // import functions
-
+import { add } from './calculations.js';
 // reference needed DOM elements
-
+const addInput1 = document.getElementById('add-num1');
+const addInput2 = document.getElementById('add-num2');
+const addAnswer = document.getElementById('add-answer');
+const addBtn = document.getElementById('add-btn');
 
 // set event listeners 
   // get user input(s)
   // do any needed work with the value(s)
   // update DOM to reflect new value(s)
+addBtn.addEventListener('click', ()=>{
+    const add1 = Number(addInput1.value);
+    const add2 = Number(addInput2.value);
+    const answer = add(add1, add2);
+    console.log(answer);
+    addAnswer.textContent = answer;
+});
