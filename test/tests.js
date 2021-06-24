360c6d567069d3ba41ced47939b46b012ee705e9
Alice-Retz <alicegretz@gmail.com>
2021-06-24 16:34:18 -0500

    finalized lab

--- test/tests.js
+++ test/tests.js
@@ -1,19 +1,8 @@
-// IMPORT MODULES under test here:
+
 import { add } from '../calculations.js';
-
 const test = QUnit.test;
-
-// name your test by what it is testing
 test('test adding', (expect) => {
-    //Arrange
-    // Set up your arguments and expectations
     const expected = 5;
-    
-    //Act 
-    // Call the function you're testing and set the result to a const
-    const actual = add(2, 3); // use your function here
-
-    //Expect
-    // Make assertions about what is expected versus the actual result
+    const actual = add(2, 3);
     expect.equal(actual, expected);
 });
